3e0f064b5bfc42b3f0142dcc2d568fd259760d4d
MK <mihael.kartashev@gmail.com>
2023-06-08 21:31:00 +0300

    disable services: DiagTrack, WSearch, wuauserv; add echo "Press Enter to reboot system..."

--- win10.disable.data.collection.cmd
+++ win10.disable.data.collection.cmd
@@ -1,16 +1,41 @@
 REM [Disable Windows data collection]
-REM Step 1. Disable AllowTelemetry
+REM Disable AllowTelemetry
 reg add "HKLM\Software\Policies\Microsoft\Windows\DataCollection" /t REG_DWORD /v AllowTelemetry /d 0 /f
 
-REM Step 2. Stop and disable diagnostic tracking service
+REM [Disable several services]
+
+REM Stop and disable diagnostic tracking service "Connected User Experiences and Telemetry"
 sc stop DiagTrack
 sc config DiagTrack start= disabled
 sc query "DiagTrack"
 
-REM Step 3. Stop and disable dmwappushservice
-sc stop dmwappushservice
-sc config dmwappushservice start= disabled
-sc query "dmwappushservice"
+REM Stop and disable WSearch service "Windows Search"
+sc stop WSearch
+sc config WSearch start= disabled
+sc query "WSearch"
+
+REM Stop and disable wuauserv service "Windows Update"
+sc stop wuauserv
+sc config wuauserv start= disabled
+sc query "wuauserv"
+
+
+
+REM sc query "DiagTrack"
+REM sc query "dmwappushservice"
+REM sc query "iphlpsvc"
+REM sc query "Spooler"
+REM sc query "ShellHWDetection"
+REM sc query "Themes"
+REM sc query "MapsBroker"
+REM sc query "wscsvc"
+REM sc query "WSearch"
+REM sc query "edgeupdate"
+REM sc query "wuauserv"
+
+echo "Press Enter to reboot system..."
+
+pause
 
 shutdown /r
 
