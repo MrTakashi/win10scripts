dbd113071d2ec4978ded68ffb46a943a8fb49da9
MK <mihael.kartashev@gmail.com>
2024-06-30 16:57:33 +0300

    update

--- office/2013/C2R/install_my_settings.cmd
+++ office/2013/C2R/install_my_settings.cmd
@@ -1,9 +1,9 @@
 REM curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/office/C2R/install_my_settings.cmd | %SystemRoot%\System32\cmd.exe
 
-echo [HKEY_CURRENT_USER\SOFTWARE\Microsoft\Office\15.0\Common\General]
-echo "DisableBootToOfficeStart"=dword:00000001
 reg add "HKCU\Software\Microsoft\Office\15.0\Common\General" /v "DisableBootToOfficeStart" /t REG_DWORD /d 1 /f
+REM [HKEY_CURRENT_USER\SOFTWARE\Microsoft\Office\15.0\Common\General]
+REM "DisableBootToOfficeStart"=dword:00000001
 
-echo [HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\SignIn]
-echo "SignInOptions"=dword:00000003
 reg add "HKCU\Software\Microsoft\Office\15.0\Common\SignIn" /v "SignInOptions" /t REG_DWORD /d 3 /f
+REM [HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\SignIn]
+REM "SignInOptions"=dword:00000003
