8a2567a14d2e1e9b4707b56aa3f9f43dfeba5bdd
MK <mihael.kartashev@gmail.com>
2024-06-30 15:25:13 +0300

    fix

--- office/C2R/install_my_settings.cmd
+++ office/C2R/install_my_settings.cmd
@@ -6,4 +6,4 @@ reg add "HKCU\Software\Microsoft\Office\15.0\Common\General" /v "DisableBootToOf
 
 echo [HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\SignIn]
 echo "SignInOptions"=dword:00000003
-reg add "HKCU\Software\Microsoft\Office\15.0\Common\General" /v "SignInOptions" /t REG_DWORD /d 3 /f
+reg add "HKCU\Software\Microsoft\Office\15.0\Common\SignIn" /v "SignInOptions" /t REG_DWORD /d 3 /f
