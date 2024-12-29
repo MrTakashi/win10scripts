c9da83867fe29d6ba3a8352b1f14338e6e95c48e
MK <mihael.kartashev@gmail.com>
2024-12-29 21:07:07 +0300

    new

---
+++ get.activated.win.cmd
@@ -0,0 +1,10 @@
+REM curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/get.activated.win.cmd | %SystemRoot%\System32\cmd.exe
+
+REM Download command
+curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/get.activated.win.cmd -OutFile "$env:TEMP\get.activated.win.ps1"
+
+REM Execute script with Power Shell
+Start-Process -FilePath "$env:TEMP\get.activated.win.ps1" -Verb RunAs -Wait
+
+REM Clear up
+Remove-Item "$env:TEMP\get.activated.win.ps1" -Force
