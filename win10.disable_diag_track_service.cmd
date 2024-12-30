cca1883694cbf280a96d458fcc93c05268d6b502
MK <mihael.kartashev@gmail.com>
2024-12-30 22:26:57 +0300

    fix

--- win10.disable_diag_track_service.cmd
+++ win10.disable_diag_track_service.cmd
@@ -1,10 +1,16 @@
 REM [Disable Windows data collection]
+
 REM Disable AllowTelemetry
 reg add "HKLM\Software\Policies\Microsoft\Windows\DataCollection" /t REG_DWORD /v AllowTelemetry /d 0 /f
 
-REM [Disable several services]
+REM [Disable service]
 
 REM Stop and disable diagnostic tracking service "Connected User Experiences and Telemetry"
 sc stop DiagTrack
 sc config DiagTrack start= disabled
 sc query "DiagTrack"
+
+echo "Press any key to continue..."
+pause
+
+REM Based on: https://www.pdq.com/blog/remotely-disable-windows-data-collection/
