e68a7735f38c0e26d19b87d7ebb6ae56ed05baff
MK <mihael.kartashev@gmail.com>
2024-12-30 22:13:23 +0300

    ref.

--- get.activated.win.cmd
+++ get.activated.win.cmd
@@ -1,10 +1,20 @@
 REM curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/get.activated.win.cmd | %SystemRoot%\System32\cmd.exe
 
-REM Download command
-curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/get.activated.win.cmd -OutFile "$env:TEMP\get.activated.win.ps1"
+@echo off
+REM Inform the user that the script is starting the download
+echo Downloading the PowerShell script from the URL...
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/MrTakashi/win10scripts/main/get.activated.win.cmd' -OutFile '%TEMP%\get.activated.win.cmd'"
+echo Download completed. The script has been saved to: %TEMP%\get.activated.win.cmd
 
-REM Execute script with Power Shell
-Start-Process -FilePath "$env:TEMP\get.activated.win.ps1" -Verb RunAs -Wait
+REM Inform the user that the script is about to run
+echo Running the downloaded script...
+powershell -Command "Start-Process -FilePath '%TEMP%\get.activated.win.cmd' -Wait"
+echo Script execution completed.
 
-REM Remove temporary file
-Remove-Item "$env:TEMP\get.activated.win.ps1" -Force
+REM Inform the user that the script file will be removed
+echo Cleaning up by removing the downloaded script...
+powershell -Command "Remove-Item -Path '%TEMP%\get.activated.win.cmd' -Force"
+echo Script file has been removed from the system.
+
+REM Please press any key...
+pause
