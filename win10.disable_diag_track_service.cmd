428521d92a25cec63fee6cffe02ba5efa00df942
MK <mihael.kartashev@gmail.com>
2025-06-04 19:02:12 +0300

    add curl command

--- win10.disable_diag_track_service.cmd
+++ win10.disable_diag_track_service.cmd
@@ -1,9 +1,10 @@
-REM [Disable Windows data collection]
+REM curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/win10.disable_diag_track_service.cmd | %SystemRoot%\System32\cmd.exe
+REM
 
-REM Disable AllowTelemetry
+REM [Disable Windows data collection]
 reg add "HKLM\Software\Policies\Microsoft\Windows\DataCollection" /t REG_DWORD /v AllowTelemetry /d 0 /f
 
-REM [Disable service]
+REM [Disable DiagTrack service]
 
 REM Stop and disable diagnostic tracking service "Connected User Experiences and Telemetry"
 sc stop DiagTrack
