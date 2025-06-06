6ac4d20399bdcbcc86e25c1539d5eb09b69434c9
MK <mihael.kartashev@gmail.com>
2025-06-06 17:31:35 +0300

    add comments

--- win10.hide.special.users.cmd
+++ win10.hide.special.users.cmd
@@ -1,8 +1,8 @@
+REM curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/win10.hide.special.users.cmd | %SystemRoot%\System32\cmd.exe
+REM
 
-REM Hide user 'parent'
+REM Hide special users 'parent', 'adm'
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /v parent /d 0 /f
-
-REM Hide user 'adm'
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /v adm /d 0 /f
 
 REM wait
--- win10.ltsc.kms.pru.cmd
+++ win10.ltsc.kms.pru.cmd
@@ -3,6 +3,11 @@ REM
 
 REM Setup KMS key and activate
 
-slmgr -ipk 92NFX-8DJQP-P6BBQ-THF9C-7CG2H
+REM GVLK for Windows Enterprise LTSC 2019, 2021, 2024
+slmgr -ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
 slmgr -skms 10.10.20.121
 slmgr -ato
+
+
+REM GVLK for Windows Enterprise LTSC 2019, 2021, 2024 (N)
+REM 92NFX-8DJQP-P6BBQ-THF9C-7CG2H
