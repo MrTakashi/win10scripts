b2e5be15738b8be72f4c0447f1a266636f323c82
MK <mihael.kartashev@gmail.com>
2025-06-07 19:48:09 +0300

    REM adm

--- win10.hide.special.users.cmd
+++ win10.hide.special.users.cmd
@@ -3,7 +3,7 @@ REM
 
 REM Hide special users 'parent', 'adm'
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /v parent /d 0 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /v adm /d 0 /f
+REM reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /v adm /d 0 /f
 
 REM wait
 echo "Press Enter to logoff..."
