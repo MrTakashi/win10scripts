d0d95bd29561294d60251d09977f6ae4af8799ae
MK <mihael.kartashev@gmail.com>
2025-06-07 20:29:08 +0300

    -echo "Press any key to continue..."

--- 01.win10.disable.diagtrack.service.cmd
+++ 01.win10.disable.diagtrack.service.cmd
@@ -11,7 +11,4 @@ sc stop DiagTrack
 sc config DiagTrack start= disabled
 sc query "DiagTrack"
 
-echo "Press any key to continue..."
-pause
-
 REM Based on: https://www.pdq.com/blog/remotely-disable-windows-data-collection/
