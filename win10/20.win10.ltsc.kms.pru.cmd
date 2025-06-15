87dc0c8688f3bbc87c6c6ba0c27d921d816b2e04
MK <mihael.kartashev@gmail.com>
2025-06-15 13:17:02 +0300

    silent mode

--- win10/20.win10.ltsc.kms.pru.cmd
+++ win10/20.win10.ltsc.kms.pru.cmd
@@ -4,9 +4,15 @@ REM
 REM Setup KMS key and activate
 
 REM GVLK for Windows Enterprise LTSC 2019, 2021, 2024
-slmgr -ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
-slmgr -skms 10.10.20.121
-slmgr -ato
+REM slmgr -ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+REM slmgr -skms 10.10.20.121
+REM slmgr -ato
+
+
+REM To avoid dialog boxes and run the commands silently
+cscript //B %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+cscript //B %windir%\system32\slmgr.vbs /skms 10.10.20.121
+cscript //B %windir%\system32\slmgr.vbs /ato
 
 
 REM GVLK for Windows Enterprise LTSC 2019, 2021, 2024 (N)
