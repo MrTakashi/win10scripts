633878f74513854e84668a034e4ec767def61099
MK <mihael.kartashev@gmail.com>
2025-08-07 23:50:48 +0300

    new line

--- win10/20.win10.ltsc.kms.pru.cmd
+++ win10/20.win10.ltsc.kms.pru.cmd
@@ -14,6 +14,5 @@ cscript //B %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
 cscript //B %windir%\system32\slmgr.vbs /skms 10.10.20.121
 cscript //B %windir%\system32\slmgr.vbs /ato
 
-
 REM GVLK for Windows Enterprise LTSC 2019, 2021, 2024 (N)
 REM 92NFX-8DJQP-P6BBQ-THF9C-7CG2H
