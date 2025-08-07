a12e87e1b2f110f332d6658a174d4f34dbc9026f
MK <mihael.kartashev@gmail.com>
2025-08-07 23:59:35 +0300

    +cscript %windir%\system32\slmgr.vbs /dli

--- win10/20.win10.ltsc.kms.pru.cmd
+++ win10/20.win10.ltsc.kms.pru.cmd
@@ -13,6 +13,7 @@ REM To avoid dialog boxes and run the commands silently
 cscript //B %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
 cscript //B %windir%\system32\slmgr.vbs /skms 10.10.20.121
 cscript //B %windir%\system32\slmgr.vbs /ato
+cscript %windir%\system32\slmgr.vbs /dli
 
 REM GVLK for Windows Enterprise LTSC 2019, 2021, 2024 (N)
 REM 92NFX-8DJQP-P6BBQ-THF9C-7CG2H
