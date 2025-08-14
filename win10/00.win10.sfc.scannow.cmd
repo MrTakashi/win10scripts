e83a4b9fa22a6b0503c27cca7dc5c18c9f978464
MK <mihael.kartashev@gmail.com>
2025-08-15 00:12:57 +0300

    new

---
+++ win10/00.win10.sfc.scannow.cmd
@@ -0,0 +1,4 @@
+REM curl https://raw.githubusercontent.com/MrTakashi/win10scripts/main/win10/00.win10.sfc.scannow.cmd | %SystemRoot%\System32\cmd.exe
+
+REM SFC is the System File Checker, it examines and replaces damaged system files.
+sfc /scannow
